73d998936b405d3df7477eefebc7819ace0406c6
b0806 <90655017+b0806@users.noreply.github.com>
2022-01-16 22:24:18 -0500

    Update VSRGEngine.java
    im commiting from github website

--- src/io/github/dthusian/ICS3UFinal/VSRGEngine.java
+++ src/io/github/dthusian/ICS3UFinal/VSRGEngine.java
@@ -3,8 +3,13 @@ package io.github.dthusian.ICS3UFinal;
 import java.io.FileReader;
 import java.io.IOException; //ioexception from osu how
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class VSRGEngine {
+    
+    public static long startTime = 0;
+    public static ArrayList<Note> notes = new ArrayList<>();
+    
     public VSRGEngine(String filepath) throws IOException, RuntimeException {
         Scanner fileReader = new Scanner(new FileReader(filepath));
         String signature = fileReader.nextLine();
