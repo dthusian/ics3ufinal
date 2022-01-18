b0d3adedc4c1e608fac892850a843cb65a6e7fca
b0806 <tambrendan05@gmail.com>
2022-01-18 01:31:31 -0500

    comments

--- src/io/github/dthusian/ICS3UFinal/Note.java
+++ src/io/github/dthusian/ICS3UFinal/Note.java
@@ -6,22 +6,23 @@ import java.util.TimerTask;
 public class Note {
     public int time = 0;
     public int lane = 0;
-    public int type = 0;
-    public int posX = 0;
+    public int type = 0; // long note/normal note
+    public int posX = 0; // probably needed for drawing them
     public int posY = 0;
-    public int endTime = 0;
+    public int endTime = 0; // only applies to long note
     NoteTimer timer;
   
     public Note(int time, int lane, int type, int endTime) {
         this.time = time;
         this.lane = lane;
-	this.endTime = endTime;
-	this.type = type;
+        this.endTime = endTime;
+        this.type = type;
       
         this.timer = new NoteTimer(this.time);
     }
 }
 
+// so that the timer thing works
 class NoteTimer extends TimerTask {
     public int time = 0;
   
