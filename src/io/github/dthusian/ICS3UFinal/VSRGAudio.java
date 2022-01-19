1b149ee4f8e3e9745d064aa5a1bfb17bcf0d7b1d
Adrian Wu <eattofueveryday@gmail.com>
2022-01-19 10:25:08 -0500

    i hate SourceDataLine

--- src/io/github/dthusian/ICS3UFinal/VSRGAudio.java
+++ src/io/github/dthusian/ICS3UFinal/VSRGAudio.java
@@ -7,56 +7,6 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class VSRGAudio {
-
-    // Class that represents a currently loaded and playing audio file
-    // Doesn't work right now
-    static class AudioStream implements Runnable {
-        SourceDataLine dataLine;
-        Thread audioThread;
-        AudioInputStream input;
-
-        public AudioStream(String path) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-            input = AudioSystem.getAudioInputStream(new File(path));
-            AudioFormat fmt = input.getFormat();
-            double bytesPerS = (double)fmt.getFrameSize() * (double)fmt.getFrameRate();
-            double bytesPerInterval = bytesPerS * 50 / 1000;
-            int roundedBufSize = (int)Math.round(bytesPerInterval / (double)fmt.getFrameSize() * fmt.getFrameSize());
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, fmt, roundedBufSize);
-            dataLine = (SourceDataLine) AudioSystem.getLine(info);
-            audioThread = new Thread(this);
-        }
-
-        public void start() {
-            dataLine.start();
-            audioThread.start();
-        }
-
-        public double getSecondPosition() {
-            return this.dataLine.getMicrosecondPosition() / 1000000.0;
-        }
-
-        public void pause() {
-            dataLine.stop();
-        }
-
-        public void resume() {
-            dataLine.start();
-        }
-
-        @Override
-        public void run() {
-            try {
-                while(true) {
-                    byte[] buf = new byte[dataLine.getBufferSize()];
-                    int read = input.read(buf);
-                    dataLine.write(buf, 0, buf.length);
-                    if(read == 0) break;
-                }
-            } catch(IOException ignored) {
-            }
-        }
-    }
-
     // internal cache of sound effects
     HashMap<String, Clip> sfxs;
 
@@ -90,7 +40,9 @@ public class VSRGAudio {
 
     // loads a longer audio file for playing
     // note that shorter files should be played with the sfx methods
-    public AudioStream loadMusic(String path) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
-        return new AudioStream(path);
+    public Clip loadMusic(String path) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        Clip clip = AudioSystem.getClip();
+        clip.open(AudioSystem.getAudioInputStream(new File(path)));
+        return clip;
     }
 }
