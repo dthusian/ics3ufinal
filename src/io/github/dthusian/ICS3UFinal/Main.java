6529da3f66343e1383de07dda14e7524d132cb2e
b0806 <tambrendan05@gmail.com>
2022-01-20 10:16:48 -0500

    fullscreen

--- src/io/github/dthusian/ICS3UFinal/Main.java
+++ src/io/github/dthusian/ICS3UFinal/Main.java
@@ -12,6 +12,8 @@ public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
     	JFrame frame = new JFrame("CS Mania");
         frame.add(new Menu());
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //frame.setUndecorated(true);
         frame.pack();
         frame.setVisible(true);
     }
