db11f7bb26dcb28712d6290cd6f4ca0c7f9cef0f
Adrian Wu <eattofueveryday@gmail.com>
2022-01-21 11:41:18 -0500

    vsrgrenderer

--- src/io/github/dthusian/ICS3UFinal/Menu.java
+++ src/io/github/dthusian/ICS3UFinal/Menu.java
@@ -13,6 +13,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Menu extends JPanel implements MouseListener, KeyListener, Runnable {
   public static final int MENU_MAIN = 0;
@@ -20,12 +22,13 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
   public static final int MENU_GAME = 2;
   public static final int MENU_CREDITS = 3;
   public static final int MENU_GAME_RESULTS = 4;
-  int currentMenu = 2;
+  int currentMenu = 0;
   
   boolean setupDone = false; // so that some things dont have to be redone every frame
 
-  Song song = null;
+  ArrayList<Song> songs = new ArrayList<>();
   VSRGEngine engine = null;
+  VSRGRenderer render = null;
   
   boolean[] keysPressed = new boolean[4];
   
@@ -36,6 +39,30 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
     this.setFocusable(true);
     addMouseListener(this);
     addKeyListener(this);
+    new Thread(this).start();
+  }
+
+  private void loadSongs() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+      String songsFolderPath = "src\\io\\github\\dthusian\\ICS3UFinal\\songs";
+      File songsFolder = new File(songsFolderPath);
+      File[] listFiles = songsFolder.listFiles();
+      if(listFiles == null) {
+          throw new RuntimeException("Songs folder not present");
+      }
+      for (int i = 0; i < listFiles.length; i++) {
+          // get each song inside songs folder
+          File[] innerFiles = listFiles[i].listFiles();
+          boolean foundOsuFile = false;
+          for (int j = 0; j < innerFiles.length; j++) {
+              if (innerFiles[j].getName().substring(innerFiles[j].getName().length() - 4).equals(".osu")) {
+                  foundOsuFile = true;
+                  songs.add(new Song(innerFiles[j].getAbsolutePath()));
+              }
+          }
+          if (!foundOsuFile) {
+              throw new FileNotFoundException("no .osu file");
+          }
+      }
   }
 
   private void drawButton(Graphics g, Color col, String text, int baseX, int baseY, int width, int height, int slant, Point mousePos) {
@@ -71,51 +98,26 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
     g2d.setColor(new Color(240, 240, 240));
     g2d.setFont(new Font("sans-serif", Font.PLAIN, 50));
     g2d.drawString("CS Mania", 50, 400);
-    new Thread(this).start();
   }
   
-  public void drawSongSelect(Graphics g) throws IOException {
+  public void drawSongSelect(Graphics g) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 	  Graphics2D g2d = (Graphics2D)g;
 	  g2d.setPaint(new GradientPaint(0, 0, new Color(0, 0, 99), 0, this.getHeight(), new Color(9, 0, 173)));
 	  g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 	  FileLoader fileLoader = new FileLoader();
 	  
 	  // get songs folder
-	  String songsFolderPath = "src\\io\\github\\dthusian\\ICS3UFinal\\songs";
-	  File songsFolder = new File(songsFolderPath);
-	  File[] songsFiles = songsFolder.listFiles();
-	  
-	  /*
-	  // load map background as song select background
-	  try {
-		  BufferedImage image = ImageIO.read(new File(songFolderPath + "\\" + songFiles[0].getName()));
-	  } catch (IOException e) {
-		  
-	  }
-	  */
+      if(songs.size() == 0) {
+          loadSongs();
+      }
 	  
 	  Point p = myGetMousePosition();
 	  int scroll = 50;
-	  for (int i = 0; i < songsFiles.length; i++) {
-		  
-		  // get each song inside songs folder
-		  File songFolder = new File(songsFolderPath + "\\" + songsFiles[i].getName());
-		  File[] songFiles = songFolder.listFiles();
-		  String osuFilePath = null;
-		  for (int j = 0; j < songFiles.length; j++) {
-			  if (songFiles[j].getName().substring(songFiles[j].getName().length() - 4).equals(".osu")) {
-				  osuFilePath = songsFolderPath + "\\" + songFolder.getName() + "\\" + songFiles[j].getName();
-			  }
-		  }
-		  if (osuFilePath == null) {
-			  throw new FileNotFoundException("no .osu file");
-		  }
-		  
-		  drawButton(g2d, new Color(159, 64, 255), fileLoader.getTitleArtist(osuFilePath)[1] + " - " + fileLoader.getTitleArtist(osuFilePath)[0], 50, scroll, 200, 50, 20, p);
+	  for (int i = 0; i < songs.size(); i++) {
+          HashMap<String, String> metadata = songs.get(i).metadata;
+		  drawButton(g2d, new Color(159, 64, 255), metadata.get("Artist") + " - " + metadata.get("Title") + " [" + metadata.get("Version") + "] ", 50, scroll, 200, 50, 20, p);
 		  scroll += 100;
 	  }
-	  
-	  new Thread(this).start();
   }
 
   public void drawGame(Graphics g) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
@@ -126,67 +128,15 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
 	  
 	  if (!setupDone) {
 	  // testing purposes
-		  this.song = new Song("src\\io\\github\\dthusian\\ICS3UFinal\\songs\\psimissing\\Mami Kawada - PSI-missing (TV Size) (PotatoDew) [[4K] Insane].osu");
-		  this.song.dimBg(0.5f, 0.0f);
-		  this.engine = new VSRGEngine(this.song);
+		  Song song = new Song("src\\io\\github\\dthusian\\ICS3UFinal\\songs\\psimissing\\Mami Kawada - PSI-missing (TV Size) (PotatoDew) [[4K] Insane].osu");
+          song.dimBg(0.5f, 0.0f);
+		  this.engine = new VSRGEngine(song);
 		  setupDone = true;
 		  System.out.println("done");
 	  }
-	
-	  g.drawImage(this.song.background, 0, 0, null);
-	  gameLayout(g2d);
-	  
-	  if (this.keysPressed[0]) {
-		  g.setColor(new Color(250, 180, 240));
-		  g.fillRect(this.getWidth()/2 - 145, this.getHeight() - 80, 70, 30);
-	  }
-	  if (this.keysPressed[1]) {
-		  g.setColor(new Color(120, 200, 230));
-		  g.fillRect(this.getWidth()/2 - 70, this.getHeight() - 80, 70, 30);
-	  }
-	  if (this.keysPressed[2]) {
-		  g.setColor(new Color(255, 230, 130));
-		  g.fillRect(this.getWidth()/2 + 5, this.getHeight() - 80, 70, 30);
-	  }
-	  if (this.keysPressed[3]) {
-		  g.setColor(new Color(160, 230, 150));
-		  g.fillRect(this.getWidth()/2 + 80, this.getHeight() - 80, 70, 30); 
-	  }
-	  
+
+	  render.draw(this, g);
 	  engine.tick();
-	  
-	  new Thread(this).start();
-  }
-  
-  public void gameLayout(Graphics g) {
-	  g.setColor(new Color(124, 124, 124));
-	  g.fillRect(this.getWidth()/2 - 150, 0, 300, this.getHeight() - 85);
-	  g.setColor(new Color(211, 211, 211));
-	  g.fillRect(this.getWidth()/2 - 150, this.getHeight() - 85, 300, 35);
-	  
-	  g.setColor(new Color(250, 180, 240));
-	  g.fillRect(this.getWidth()/2 - 150, this.getHeight() - 50, 78, 50);
-	  g.setColor(new Color(160, 230, 150));
-	  g.fillRect(this.getWidth()/2 + 78, this.getHeight() - 50, 75, 50);
-	  g.setColor(new Color(120, 200, 230));
-	  g.fillRect(this.getWidth()/2 - 72, this.getHeight() - 50, 75, 50);
-	  g.setColor(new Color(255, 230, 130));
-	  g.fillRect(this.getWidth()/2 + 3, this.getHeight() - 50, 75, 50);
-	  
-	  g.setColor(new Color(0, 0, 0));
-	  g.fillRect(this.getWidth()/2 - 150, 0, 5, this.getHeight());
-	  g.fillRect(this.getWidth()/2 - 75, 0, 5, this.getHeight() - 50);
-	  g.fillRect(this.getWidth()/2, 0, 5, this.getHeight() - 50);
-	  g.fillRect(this.getWidth()/2 + 75, 0, 5, this.getHeight() - 50);
-	  g.fillRect(this.getWidth()/2 + 150, 0, 5, this.getHeight());
-	  g.fillRect(this.getWidth()/2 - 150, this.getHeight() - 85, 300, 5);
-	  g.fillRect(this.getWidth()/2 - 150, this.getHeight() - 50, 300, 5);
-	  
-	  g.setFont(new Font("sans-serif", Font.PLAIN, 52));
-	  g.drawString("0", 20, 50);
-	  g.setFont(new Font("sans-serif", Font.PLAIN, 50));
-	  g.setColor(new Color(255, 255, 255));
-	  g.drawString("0", 20, 50);
   }
   
   public void paintComponent(Graphics g) {
@@ -196,7 +146,7 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
     } else if (currentMenu == MENU_SONG_SELECT) {
     	try {
 			drawSongSelect(g);
-		} catch (IOException e) {
+		} catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
 			e.printStackTrace();
 		}
     } else if (currentMenu == MENU_GAME) {
@@ -229,18 +179,15 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
   public void keyPressed(KeyEvent e) {
 	  if (currentMenu == MENU_GAME) {
 		  if (e.getKeyChar() == 'd') {
-			  keysPressed[0] = true;
-			  repaint();
+              render.keyPress(0);
 		  } else if (e.getKeyChar() == 'f') {
-			  keysPressed[1] = true;
-			  repaint();
+              render.keyPress(1);
 		  } else if (e.getKeyChar() == 'j') {
-			  keysPressed[2] = true;
-			  repaint();
+              render.keyPress(2);
 		  } else if (e.getKeyChar() == 'k') {
-			  keysPressed[3] = true;
-			  repaint();
+              render.keyPress(3);
 		  }
+          repaint();
 	  }
   }
 
@@ -248,18 +195,15 @@ public class Menu extends JPanel implements MouseListener, KeyListener, Runnable
   public void keyReleased(KeyEvent e) {
 	  if (currentMenu == MENU_GAME) {
 		  if (e.getKeyChar() == 'd') {
-			  keysPressed[0] = false;
-			  repaint();
+              render.keyPress(0);
 		  } else if (e.getKeyChar() == 'f') {
-			  keysPressed[1] = false;
-			  repaint();
+              render.keyPress(1);
 		  } else if (e.getKeyChar() == 'j') {
-			  keysPressed[2] = false;
-			  repaint();
+              render.keyPress(2);
 		  } else if (e.getKeyChar() == 'k') {
-			  keysPressed[3] = false;
-			  repaint();
+              render.keyPress(3);
 		  }
+          repaint();
 	  }
   }
 
---
+++ src/io/github/dthusian/ICS3UFinal/VSRGRenderer.java
@@ -0,0 +1,71 @@
+package io.github.dthusian.ICS3UFinal;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VSRGRenderer {
+    VSRGEngine eng;
+    boolean[] keysPressed;
+
+    public VSRGRenderer(VSRGEngine eng) {
+        this.eng = eng;
+        keysPressed = new boolean[4];
+    }
+
+    public void keyPress(int lane) {
+        keysPressed[lane] = true;
+    }
+
+    public void keyRelease(int lane) {
+        keysPressed[lane] = false;
+    }
+
+    public void draw(JPanel panel, Graphics g) {
+        g.drawImage(eng.currentSong.background, 0, 0, null);
+        g.setColor(new Color(124, 124, 124));
+        g.fillRect(panel.getWidth()/2 - 150, 0, 300, panel.getHeight() - 85);
+        g.setColor(new Color(211, 211, 211));
+        g.fillRect(panel.getWidth()/2 - 150, panel.getHeight() - 85, 300, 35);
+
+        g.setColor(new Color(250, 180, 240));
+        g.fillRect(panel.getWidth()/2 - 150, panel.getHeight() - 50, 78, 50);
+        g.setColor(new Color(160, 230, 150));
+        g.fillRect(panel.getWidth()/2 + 78, panel.getHeight() - 50, 75, 50);
+        g.setColor(new Color(120, 200, 230));
+        g.fillRect(panel.getWidth()/2 - 72, panel.getHeight() - 50, 75, 50);
+        g.setColor(new Color(255, 230, 130));
+        g.fillRect(panel.getWidth()/2 + 3, panel.getHeight() - 50, 75, 50);
+
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(panel.getWidth()/2 - 150, 0, 5, panel.getHeight());
+        g.fillRect(panel.getWidth()/2 - 75, 0, 5, panel.getHeight() - 50);
+        g.fillRect(panel.getWidth()/2, 0, 5, panel.getHeight() - 50);
+        g.fillRect(panel.getWidth()/2 + 75, 0, 5, panel.getHeight() - 50);
+        g.fillRect(panel.getWidth()/2 + 150, 0, 5, panel.getHeight());
+        g.fillRect(panel.getWidth()/2 - 150, panel.getHeight() - 85, 300, 5);
+        g.fillRect(panel.getWidth()/2 - 150, panel.getHeight() - 50, 300, 5);
+
+        g.setFont(new Font("sans-serif", Font.PLAIN, 52));
+        g.drawString("0", 20, 50);
+        g.setFont(new Font("sans-serif", Font.PLAIN, 50));
+        g.setColor(new Color(255, 255, 255));
+        g.drawString("0", 20, 50);
+
+        if (this.keysPressed[0]) {
+            g.setColor(new Color(250, 180, 240));
+            g.fillRect(panel.getWidth()/2 - 145, panel.getHeight() - 80, 70, 30);
+        }
+        if (this.keysPressed[1]) {
+            g.setColor(new Color(120, 200, 230));
+            g.fillRect(panel.getWidth()/2 - 70, panel.getHeight() - 80, 70, 30);
+        }
+        if (this.keysPressed[2]) {
+            g.setColor(new Color(255, 230, 130));
+            g.fillRect(panel.getWidth()/2 + 5, panel.getHeight() - 80, 70, 30);
+        }
+        if (this.keysPressed[3]) {
+            g.setColor(new Color(160, 230, 150));
+            g.fillRect(panel.getWidth()/2 + 80, panel.getHeight() - 80, 70, 30);
+        }
+    }
+}
