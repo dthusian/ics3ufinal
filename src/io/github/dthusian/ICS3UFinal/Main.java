73334afff20e8fcd139d14cc67fdb3ae2b3ec869
Adrian Wu <eattofueveryday@gmail.com>
2022-01-24 10:11:59 -0500

    I still hate SourceDataLine (but at least it works)

--- src/io/github/dthusian/ICS3UFinal/Main.java
+++ src/io/github/dthusian/ICS3UFinal/Main.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException, InterruptedException {
-    	JFrame frame = new JFrame("CS Mania");
-        frame.add(new Menu());
-        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //frame.setUndecorated(true);
-        frame.pack();
-        frame.setVisible(true);
+    	VSRGAudio.AudioStream as = VSRGAudio.loadMusic2("run/xnor.wav");
+        Thread.sleep(10000);
     }
 }
--- src/io/github/dthusian/ICS3UFinal/VSRGAudio.java
+++ src/io/github/dthusian/ICS3UFinal/VSRGAudio.java
@@ -1,12 +1,35 @@
 package io.github.dthusian.ICS3UFinal;
 
 import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.Buffer;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class VSRGAudio {
+    static class AudioStream {
+        public Thread thread;
+        public AtomicInteger signal;
+
+        AudioStream(Thread t, AtomicInteger i) {
+            thread = t;
+            signal = i;
+        }
+
+        public void pause() {
+            signal.set(1);
+        }
+
+        public void resume() {
+            signal.set(0);
+        }
+
+        public void stop() {
+            signal.set(2);
+        }
+    }
+
     // internal cache of sound effects
     static HashMap<String, Clip> sfxs = new HashMap<>();
 
@@ -40,5 +63,45 @@ public class VSRGAudio {
         Clip clip = AudioSystem.getClip();
         clip.open(AudioSystem.getAudioInputStream(new File(path)));
         return clip;
+    }
+
+    // attempt 2
+    public static AudioStream loadMusic2(String path) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(new File(path))));
+        AudioFormat audioFormat = ais.getFormat();
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open(audioFormat);
+        line.start();
+        AtomicInteger signal = new AtomicInteger(0);
+        Thread audioThread = new Thread(() -> {
+            try {
+                byte[] samples = new byte[4096];
+                int count = 0;
+                while ((count = ais.read(samples, 0, 4096)) != -1) {
+                    line.write(samples, 0, count);
+                    int signalToken = signal.get();
+                    if(signalToken != 0) {
+                        if(signalToken == 1) {
+                            while(signalToken == 1) {
+                                Thread.sleep(1000);
+                                signalToken = signal.get();
+                            }
+                        } else if(signalToken == 2) {
+                            break;
+                        }
+                    }
+                }
+                line.drain();
+                line.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        audioThread.start();
+        AudioStream as = new AudioStream(audioThread, signal);
+        return as;
     }
 }
