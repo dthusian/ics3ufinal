75384a350ee0d8c739d314eaad2631578d717f3d
Adrian Wu <eattofueveryday@gmail.com>
2022-01-26 23:41:05 -0500

    continue commenting

--- src/io/github/dthusian/ICS3UFinal/VSRGRenderer.java
+++ src/io/github/dthusian/ICS3UFinal/VSRGRenderer.java
@@ -5,6 +5,12 @@ import java.awt.*;
 import java.awt.font.FontRenderContext;
 
 public class VSRGRenderer {
+    final Color[] laneColors = new Color[]{
+            new Color(250, 180, 240),
+            new Color(120, 200, 230),
+            new Color(255, 230, 130),
+            new Color(160, 230, 150)
+    };
     VSRGEngine eng;
     boolean[] keysPressed;
 
@@ -24,29 +30,21 @@ public class VSRGRenderer {
     }
 
     public void drawLane(JPanel panel, Graphics2D g, int xBase, int laneId, Color col) {
-        g.setColor(col);
-        if (this.keysPressed[laneId]) {
-            g.setColor(col);
-        } else {
-            g.setColor(new Color(70, 70, 70));
-        }
-        g.fillRect(panel.getWidth() / 2 + xBase, panel.getHeight() - 80, 75, 80);
         g.setColor(new Color(150, 150, 150));
         g.drawRect(panel.getWidth() / 2 + xBase, 0, 75, panel.getHeight());
+        g.drawLine(panel.getWidth() / 2 + xBase, panel.getHeight() - 100, panel.getWidth() / 2 + xBase + 75, panel.getHeight() - 100);
+        if(keysPressed[laneId]) {
+            g.setPaint(new GradientPaint(0, panel.getHeight() - 120, new Color(laneColors[laneId].getRed(), laneColors[laneId].getGreen(), laneColors[laneId].getBlue(), 0), 0, panel.getHeight() - 100, laneColors[laneId]));
+            g.fillRect(panel.getWidth() / 2 + xBase, panel.getHeight() - 120, 75, 20);
+        }
     }
 
     public void draw(JPanel panel, Graphics g) {
-        final Color[] laneColors = new Color[]{
-                new Color(250, 180, 240),
-                new Color(120, 200, 230),
-                new Color(255, 230, 130),
-                new Color(160, 230, 150)
-        };
 
         // Draw background
         g.drawImage(eng.currentSong.background, 0, 0, null);
         g.setColor(new Color(40, 40, 40));
-        g.fillRect(panel.getWidth() / 2 - 150, 0, 300, panel.getHeight() - 80);
+        g.fillRect(panel.getWidth() / 2 - 150, 0, 300, panel.getHeight());
 
         // Draw lanes
         Graphics2D g2d = (Graphics2D) g;
@@ -67,8 +65,8 @@ public class VSRGRenderer {
             Note currentNote = eng.currentSong.notes.get(i);
             final int NOTE_THICKNESS = 30;
             final double APPROACH_RATE = 1.4;
-            int posY = (int) ((panel.getHeight() - 80 /* hitline position */) - (currentNote.time - time /* time to line */) * APPROACH_RATE);
-            int farY = (int) ((panel.getHeight() - 80 /* hitline position */) - (currentNote.endTime - time /* time to line */) * APPROACH_RATE);
+            int posY = (int) ((panel.getHeight() - 100 /* hitline position */) - (currentNote.time - time /* time to line */) * APPROACH_RATE);
+            int farY = (int) ((panel.getHeight() - 100 /* hitline position */) - (currentNote.endTime - time /* time to line */) * APPROACH_RATE);
             if (currentNote.clickState == 0 || currentNote.clickState == 2) {
                 g.setColor(laneColors[currentNote.lane]);
             } else if (currentNote.clickState == 3) {
@@ -100,7 +98,7 @@ public class VSRGRenderer {
         if (setJudgement) {
             g2d.setFont(new Font("sans-serif", Font.BOLD, 25));
             g2d.setColor(judgementCol);
-            g2d.drawString(judgementStr, (int) (panel.getWidth() / 2 - g2d.getFont().getStringBounds(judgementStr, g2d.getFontRenderContext()).getWidth() / 2), panel.getHeight() - 100);
+            g2d.drawString(judgementStr, (int) (panel.getWidth() / 2 - g2d.getFont().getStringBounds(judgementStr, g2d.getFontRenderContext()).getWidth() / 2), panel.getHeight() - 120);
         }
     }
 }
